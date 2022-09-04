9f8922f28f6af1ff66b2fc52799375855720d93e
Prashriya <prashriyaacharya02@gmail.com>
2022-09-04 12:33:39 +0545

    Created Background Style

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final imageUrl = "https://www.pinterest.com/pin/759630662155055181/";
     return Drawer(
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
             padding: EdgeInsets.zero,
@@ -12,8 +15,46 @@ class MyDrawer extends StatelessWidget {
               margin: EdgeInsets.zero,
               accountName: Text("Prashriya Acharya"),
               accountEmail: Text("sgdhu@gmail.com"),
+              currentAccountPicture:
+                  CircleAvatar(backgroundImage: NetworkImage(imageUrl)),
             ),
-          )
+          ),
+          ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.teal,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.teal,
+                ),
+              )),
+          ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.teal,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.teal,
+                ),
+              )),
+          ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.teal,
+              ),
+              title: Text(
+                "Email Me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.teal,
+                ),
+              ))
         ],
       ),
     );
